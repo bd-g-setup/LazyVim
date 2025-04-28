1b0a24a42a8590e5810dc02fc12e566a9a0e3289
Brent George <me@brentdgeorge.com>
2025-04-27 21:05:22 -0400

    don't have clock output

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -156,11 +156,6 @@ return {
             { "progress", separator = " ", padding = { left = 1, right = 0 } },
             { "location", padding = { left = 0, right = 1 } },
           },
-          lualine_z = {
-            function()
-              return " " .. os.date("%R")
-            end,
-          },
         },
         extensions = { "neo-tree", "lazy", "fzf" },
       }
